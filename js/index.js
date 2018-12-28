ee328bf97e58a18487febd8f9d3c61410e6ffdb4
Robert Snyder <r.snyder49@gmail.com>
2018-12-28 21:49:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -33,12 +33,12 @@ function getCommits(ele) {
   xhr.open("GET", uri);
   xhr.send();
 }
-// function displayCommits() {
-//   const commits = JSON.parse(this.responseText);
-//   const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
-// ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`;
-//   document.getElementById("details").innerHTML = commitsList;
-// }
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
+' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = commitsList;
+}
  
 //   function getBranches(el) {
 //   const repoName = el.dataset.repository;
