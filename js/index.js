0ab2ea754f45cd786c6303b9ab2fb265c3db04ff
Robert Snyder <r.snyder49@gmail.com>
2018-12-28 21:53:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -50,8 +50,8 @@ function getBranches(ele) {
   req.send();
 }
  
-//   function displayBranches() {
-//   const branches = JSON.parse(this.responseText);
-//   const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`;
-//   document.getElementById("details").innerHTML = branchesList;
-// } 
+  function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = branchesList;
+} 
